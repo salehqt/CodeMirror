336ca252f46038acb1d4dd673452168858d77af7
Saleh Dindar <saleh@cise.ufl.edu>
2013-06-27 09:22:55 -0400

    adding sample lint module to CodeMirror.

---
+++ addon/lint/yakand-lint.js
@@ -0,0 +1,101 @@
+(function() {
+
+  var bogus = [ "Dangerous comment" ];
+
+  var warnings = [ [ "Expected '{'",
+                     "Statement body should be inside '{ }' braces." ] ];
+
+  var errors = [ "Missing semicolon", "Extra comma", "Missing property name",
+                 "Unmatched ", " and instead saw", " is not defined",
+                 "Unclosed string", "Stopping, unable to continue" ];
+
+  function validator(options, text) {
+    var errors = [ { line: 1, character: 2, reason: "because I said so", severity: "error" } ], result = [];
+    parseErrors([], result);
+    return result;
+  }
+
+  CodeMirror.yakandValidatorWithOptions = function(options) {
+    return function(text) { return validator(options, text); };
+  };
+
+  CodeMirror.yakandValidator = CodeMirror.yakandValidatorWithOptions(null);
+
+  
+  function parseErrors(errors, output) {
+	console.log("i am here");
+    for ( var i = 0; i < errors.length; i++) {
+      var error = errors[i];
+      if (error) {
+        var linetabpositions, index;
+
+        linetabpositions = [];
+
+        // This next block is to fix a problem in jshint. Jshint
+        // replaces
+        // all tabs with spaces then performs some checks. The error
+        // positions (character/space) are then reported incorrectly,
+        // not taking the replacement step into account. Here we look
+        // at the evidence line and try to adjust the character position
+        // to the correct value.
+        if (error.evidence) {
+          // Tab positions are computed once per line and cached
+          var tabpositions = linetabpositions[error.line];
+          if (!tabpositions) {
+            var evidence = error.evidence;
+            tabpositions = [];
+            // ugggh phantomjs does not like this
+            // forEachChar(evidence, function(item, index) {
+            Array.prototype.forEach.call(evidence, function(item,
+                                                            index) {
+              if (item === '\t') {
+                // First col is 1 (not 0) to match error
+                // positions
+                tabpositions.push(index + 1);
+              }
+            });
+            linetabpositions[error.line] = tabpositions;
+          }
+          if (tabpositions.length > 0) {
+            var pos = error.character;
+            tabpositions.forEach(function(tabposition) {
+              if (pos > tabposition) pos -= 1;
+            });
+            error.character = pos;
+          }
+        }
+
+        var start = error.character - 1, end = start + 1;
+        if (error.evidence) {
+          index = error.evidence.substring(start).search(/.\b/);
+          if (index > -1) {
+            end += index;
+          }
+        }
+
+        // Convert to format expected by validation service
+        error.description = error.reason;// + "(jshint)";
+        error.start = error.character;
+        error.end = end;
+
+        if (error)
+          output.push({message: error.description,
+                       severity: error.severity,
+                       from: CodeMirror.Pos(error.line - 1, start),
+                       to: CodeMirror.Pos(error.line - 1, end)});
+      }
+	  
+    }
+  
+	output.push({message: "Hey You",
+			   severity: "error",
+			   from: CodeMirror.Pos(1, 1),
+			   to: CodeMirror.Pos(1, 2)});
+			   output.push({message: "i AM WARNING YOU",
+			   severity: "warning",
+			   from: CodeMirror.Pos(2, 1),
+			   to: CodeMirror.Pos(2, 2)});
+
+    console.dir(output);
+  }
+})();
